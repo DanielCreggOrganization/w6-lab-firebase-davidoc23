414390139ca82c6c51e53c76b9ea25c2b41b9f5b
David O Connor <123404234+davidoc23@users.noreply.github.com>
2024-10-24 09:20:24 +0000

    Add Firebase configuration and dependencies; update .gitignore and main.ts

--- w6-lab-firebase/src/main.ts
+++ w6-lab-firebase/src/main.ts
@@ -4,11 +4,14 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({"projectId":"fir-ionic-project-883d7","appId":"1:48854513740:web:1f64aa81b45ab1bb7c5ce9","storageBucket":"fir-ionic-project-883d7.appspot.com","apiKey":"AIzaSyAvOduobh0RiKaSM2UFvtYS-Ra5YUmf2F8","authDomain":"fir-ionic-project-883d7.firebaseapp.com","messagingSenderId":"48854513740"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 });
